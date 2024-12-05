b6c49289c141db4c77f26b25f9d1718f7494eaa2
Myriam <ikdgk@plaiaundi.net>
2024-12-05 13:12:12 +0100

    ramaNueva2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Rama Nueva!");
+        System.out.println("Hello, Rama Nueva2!");
     }
 }
