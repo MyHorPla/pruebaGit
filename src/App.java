fd369f4cec717499640d8c7dfa06f0142a5b6737
Arkaitz Amostegui <ikdge@plaiaundi.net>
2024-12-11 10:22:34 +0100

    Primer commit Arkaitz

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Rama Nueva2!");
+        System.out.println("Hola Myriam");
     }
 }
